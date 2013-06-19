f7631ad95f50866752eb3c3273a1d65b5d6e8fd0
Jacob Flores <jacobflores562@gmail.com>
2013-06-19 01:02:30 -0700

    Changing mind record to pass push data to processing.

--- MindRecord.java
+++ MindRecord.java
@@ -1,3 +1,112 @@
+<<<<<<< HEAD
+import com.sun.jna.ptr.IntByReference;
+import java.util.Hashtable;
+import java.lang.StringBuilder;
+import java.util.Date;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+
+public class MindRecord {
+
+	public static void recordStart() {
+		Pointer eEvent			= Edk.INSTANCE.EE_EmoEngineEventCreate();
+    	Pointer eState			= Edk.INSTANCE.EE_EmoStateCreate();
+    	// IntByReference userID 	= null;
+    	//3008 = control panel connect, connecting directly to device seems to mean you have to do profile management manaully. screw that.
+    	short composerPort		= 3008;
+    	int state  				= 0;
+    	Date startTime 			= new Date();
+    	Date now				= new Date();
+    	String delimeter		= " ";
+    	StringBuilder timeStamp;
+
+    	if (Edk.INSTANCE.EE_EngineRemoteConnect("127.0.0.1", composerPort, "Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
+				System.out.println("Cannot connect to EmoComposer on [127.0.0.1]");
+				return;
+		}
+		System.out.println("Connected to ControlPanel on [127.0.0.1]");
+			DisplayFrame processing = new DisplayFrame();
+			processing.setVisible(true);
+			while (now.getMinutes() - startTime.getMinutes() < 3) {
+				timeStamp = new StringBuilder();
+				state = Edk.INSTANCE.EE_EngineGetNextEvent(eEvent);
+
+				// New event needs to be handled
+				if (state == EdkErrorCode.EDK_OK.ToInt()) {
+
+					int eventType = Edk.INSTANCE.EE_EmoEngineEventGetType(eEvent);
+					// Edk.INSTANCE.EE_EmoEngineEventGetUserId(eEvent, userID);
+					int cogType = Edk.INSTANCE.EE_CognitivEventGetType(eEvent);
+					System.out.println(cogType);
+
+					// Log the EmoState if it has been updated
+					if (eventType == Edk.EE_Event_t.EE_EmoStateUpdated.ToInt()) {
+
+						Edk.INSTANCE.EE_EmoEngineEventGetEmoState(eEvent, eState);
+						
+						int action = EmoState.INSTANCE.ES_CognitivGetCurrentAction(eState);
+						double power = EmoState.INSTANCE.ES_CognitivGetCurrentActionPower(eState);
+						if(power!=0){
+						
+							System.out.println("action: " + action + "\npower: "+ power);
+							if (action == 2) {
+								processing.increase();
+								processing.increase();
+							} else {
+								processing.decrease();
+							}
+
+						}
+						// System.out.print("WirelessSignalStatus: ");
+						// System.out.println(EmoState.INSTANCE.ES_GetWirelessSignalStatus(eState));
+						
+						// if (EmoState.INSTANCE.ES_ExpressivIsBlink(eState) == 1)
+						// 	System.out.println("Blink");
+						// if (EmoState.INSTANCE.ES_ExpressivIsLeftWink(eState) == 1)
+						// 	System.out.println("LeftWink");
+						// if (EmoState.INSTANCE.ES_ExpressivIsRightWink(eState) == 1) {
+						//   	System.out.println("RightWink");
+						  	
+						// }
+						// if (EmoState.INSTANCE.ES_ExpressivIsLookingLeft(eState) == 1)
+						// 	System.out.println("LookingLeft");
+						// if (EmoState.INSTANCE.ES_ExpressivIsLookingRight(eState) == 1)
+						// 	System.out.println("LookingRight");
+						
+						//for printing line by line, just pipe to some file	
+
+						// Max Commented this:
+						// timeStamp.append(EmoState.INSTANCE.ES_AffectivGetExcitementShortTermScore(eState) + delimeter);
+						// timeStamp.append(EmoState.INSTANCE.ES_AffectivGetExcitementLongTermScore(eState) + delimeter);
+						// timeStamp.append(EmoState.INSTANCE.ES_AffectivGetEngagementBoredomScore(eState) + delimeter);			
+						// timeStamp.append(EmoState.INSTANCE.ES_AffectivGetMeditationScore(eState) + delimeter);
+						// timeStamp.append(EmoState.INSTANCE.ES_AffectivGetFrustrationScore(eState) + delimeter);
+
+						// now = new Date();
+						// timeStamp.append(now);
+
+						// System.out.println(timeStamp);
+
+						//for exporting to json
+
+
+					}
+				} else if (state != EdkErrorCode.EDK_NO_EVENT.ToInt()) {
+						System.out.println("Internal error in Emotiv Engine!");
+						break;
+				}
+			}
+	    	
+	    	Edk.INSTANCE.EE_EngineDisconnect();
+	    	System.out.println("Disconnected!");
+	  
+    }
+
+	public static void main(String[] args) {
+		recordStart();
+	}
+
+=======
 import com.sun.jna.ptr.IntByReference;
 import java.util.Hashtable;
 import java.lang.StringBuilder;
@@ -85,4 +194,5 @@ public class MindRecord {
 		recordStart();
 	}
 
+>>>>>>> origin/master
 }
